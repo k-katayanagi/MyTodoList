0e3f0836d2fcdf6f15a3e9800ab7f7d965960425
k-katayanagi <kkatayanagi.ka@gmail.com>
2024-12-10 15:11:25 +0900

    【コミット】

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { ESLint } from "eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+export default {
+  ignores: ["dist"], // 無視するファイルを指定
+
+  extends: [
+    js.configs.recommended, // @eslint/js の推奨設定
+    "@typescript-eslint/recommended", // typescript-eslint の推奨設定
+    "plugin:react/recommended", // Reactの推奨設定
+    "plugin:prettier/recommended", // Prettierの推奨設定を追加
+  ],
+
+  files: ["**/*.{ts,tsx}"], // 対象とするファイルタイプ
+
+  languageOptions: {
+    ecmaVersion: 2020, // ECMAScriptバージョン
+    globals: globals.browser, // 使用するグローバル変数の定義
   },
-)
+
+  plugins: {
+    "react-hooks": reactHooks, // react-hooks プラグイン
+    "react-refresh": reactRefresh, // react-refresh プラグイン
+  },
+
+  rules: {
+    ...reactHooks.configs.recommended.rules, // React Hooksのルールを適用
+    "react-refresh/only-export-components": [
+      "warn", 
+      { allowConstantExport: true }, // React Refreshのルールを適用
+    ],
+    "prettier/prettier": "error", // PrettierのルールをESLintでエラーとして扱う
+  },
+};
